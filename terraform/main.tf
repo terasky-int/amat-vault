ba36c00a5847baf9fa0b387d03a500003a310400
shlomihai <shlomihai@payoneer.com>
2025-01-16 15:36:23 +0200

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+#######################################
+# Vault Audit
+#######################################
+
+module "vault_audit" {
+  source = "./modules/vault_audit"
+
+  audit_devices = var.audit_devices
+}
+
+#######################################
+# Vault OIDC
+#######################################
+# Link for setup: https://support.hashicorp.com/hc/en-us/articles/18678253076627-How-to-configure-Ping-Identity-OIDC-authentication-with-Vault
+
+module "vault_oidc" {
+  source = "./modules/vault_oidc"
+
+  vault_addr    = var.vault_addr
+  discovery_url = var.oidc_discovery_url
+  mount_path    = var.oidc_mount_path
+
+  client_id       = var.oidc_client_id
+  client_secret   = var.oidc_client_secret
+  bound_audiences = var.oidc_bound_audiences
+  roles           = var.oidc_roles
+}
